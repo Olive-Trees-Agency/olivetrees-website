f4c165689a1231bcaf8c05a4bdd555227951d4ec
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2022-11-28 19:38:34 +0000

    no rerender text on resize height

--- src/utils/text-split.ts
+++ src/utils/text-split.ts
@@ -1,6 +1,7 @@
 import SplitType, { SplitTypeOptions } from 'split-type';
 
 import { debounce } from './debounce';
+import { isMobile } from './media-queries';
 
 let instance: SplitType;
 const options: Partial<SplitTypeOptions> = {
@@ -8,6 +9,9 @@ const options: Partial<SplitTypeOptions> = {
   tagName: 'span',
   lineClass: 'text-line',
 };
+
+let windowHeight: number;
+let windowWidth: number;
 
 export const registerSplitText = async (onRecalculate?: CallableFunction) => {
   // Set in css for better performance.
@@ -17,9 +21,20 @@ export const registerSplitText = async (onRecalculate?: CallableFunction) => {
 
   instance = new SplitType('[text-split]', options);
 
+  windowHeight = window.innerHeight;
+  windowWidth = window.innerWidth;
+
   window.addEventListener(
     'resize',
     debounce(() => {
+      const newHeight = window.innerHeight;
+      const newWidth = window.innerWidth;
+
+      if (newWidth === windowWidth && isMobile()) {
+        windowHeight = newHeight;
+        return;
+      }
+
       instance.revert();
       instance.split(options);
       if (onRecalculate) onRecalculate();
